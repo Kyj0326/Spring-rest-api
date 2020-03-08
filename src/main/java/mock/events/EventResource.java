82f4117fe31a76e713a26672a84b35bdc96126dd
Kyj0326 <qoloa0326@gmail.com>
2020-03-08 18:34:42 +0900

    @JsonUnwrapped란?

--- src/main/java/mock/events/EventResource.java
+++ src/main/java/mock/events/EventResource.java
@@ -1,7 +1,9 @@
 package mock.events;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -19,3 +21,23 @@ public class EventResource extends EntityModel<Event> {
         add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
     }
 }
+
+//우리는 EventResource로 리턴을 했고, 이거를 오브젝트매퍼가 시리얼라이제이션 할때!
+//빈시리얼라이저를 쓴다! BeanSerializer 이거는 기본적으로 필드 이름을 사용한다. event가 다른 필드들을 가지고있는 컴포짓 객체이기때문에
+//event이름 아래에다가 다 넣어주는거임
+//근데 event로 감싸지않고 그냥 넣고싶다!
+//@JsonUnwrapped를 쓰면 꺼내준다!
+//public class EventResource extends RepresentationModel{
+//
+//    @JsonUnwrapped
+//    private Event event;
+//
+//    public EventResource(Event event){
+//        this.event=event;
+//    }
+//
+//    public Event getEvent(){
+//        return event;
+//    }
+//
+//}
